2a2d9cd257c8f4a4d32268d1c0762f757b4176d4
gatiattila <gatiattila@gmail.com>
2020-10-05 00:17:48 +0300

    Create list_tuple.py

---
+++ LIST_TUPLE/list_tuple.py
@@ -0,0 +1,30 @@
+#1. Write a python code to remove an element from a tuple.
+
+t = ("r","e","m","o","v","e","e","l","e","m","e","n","t")
+t = t[:6] + t[7:]
+print(t)
+
+#2. Replace the last element in the list with the string 'last'
+#using list comprehension and tuples
+l=[lst for lst in t[:len(t)-1]]
+l.append("last")
+print(l)
+
+#3. Extract only the strings from the following list using
+#list comprehension :slist = ['I', 'am', 1, 'list', 'of', 5, 'strings']
+slist = ['I', 'am', 1, 'list', 'of', 5, 'strings']
+slist = [ l for l in slist if type(l) == str ]
+print(slist)
+
+#4. Generate a 3 by 3 matrix that contains 'X'
+#on the main diagonal and '_' in the rest.
+matr = [ (x,y,z)
+         for x in ['X','_']
+         for y in ['X','_']
+         for z in ['X','_']
+         if ((x=='X' and y=='_' and z=='_')
+             or (x=='_' and y=='_' and z=='X')
+             or (x=='_' and y=='X' and z=='_'))]
+print(matr[0])
+print(matr[1])
+print(matr[2])
